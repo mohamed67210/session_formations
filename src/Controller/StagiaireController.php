0144684b37c80a5db176fa04bb76f7494c7e4e9a
mohamed67210 <mohamed_bechri@outlook.fr>
2023-01-08 19:53:37 +0100

    modification css

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -15,22 +15,47 @@ class StagiaireController extends AbstractController
 {
     //ajouter stagiaire
     #[Route('/stagiaire/add', name: 'add_stagiaire')]
-    public function add(ManagerRegistry $doctrine, Stagiaire $stagiaire = null,  Request $request)
+    public function add(ManagerRegistry $doctrine, Stagiaire $stagiaire = null,Session $session = null,  Request $request)
     {
         $stagiaire = new Stagiaire;
+        $session = new Session;
         $form = $this->createForm(StagiaireType::class, $stagiaire);
+        $form1 = $this->createForm(StagiaireType::class, $session);
+
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // recuperer les données de l'Employe si il existe deja et si il est nul
+            // recuperer les données de stagiaire si il existe deja et si il est nul
             $stagiaire = $form->getData();
-
+           
             // on recupere le managere doctrine
             $entityManager = $doctrine->getManager();
 
-            // persist remplace prepare en pdo , on prepare l'objet Employe 
+
+            // persist remplace prepare en pdo , on prepare l'objet stagiaire
             $entityManager->persist($stagiaire);
+
             //on execute 
             $entityManager->flush();
+
+
+            // on  retourne vers la page affichage de tous les employés
+            return $this->redirectToRoute('app_stagiaire');
+        }
+        $form1->handleRequest($request);
+        if ($form1->isSubmitted() && $form1->isValid()) {
+            // recuperer les données de stagiaire si il existe deja et si il est nul
+            $session = $form1->getData();
+           
+            // on recupere le managere doctrine
+            $entityManager = $doctrine->getManager();
+
+
+            // persist remplace prepare en pdo , on prepare l'objet stagiaire
+            $entityManager->persist($session);
+
+            //on execute 
+            $entityManager->flush();
+
 
             // on  retourne vers la page affichage de tous les employés
             return $this->redirectToRoute('app_stagiaire');
--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -4,29 +4,42 @@ namespace App\Form;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomStagiaire')
-            ->add('prenomStagiaire')
-            ->add('dateNaissanceStagiaire')
-            ->add('sexeStagiaire')
-            ->add('villeStagiaire')
-            ->add('cpStagiaire')
-            ->add('adresseStagiaire')
-            ->add('mailStagiaire')
-            ->add('telephoneStagiaire')
-            // ->add('session', EntityType::class, ['class' => Session::class, 'choice_label' => 'intituleSession', 'attr' => ['class' => 'form-control']])
-            ->add('submit', SubmitType::class, ['attr' => ['class' => 'btn btn-success']]);
-        ;
+            ->add('nomStagiaire', TextType::class, ['attr' => ['class' => 'input']])
+            ->add('prenomStagiaire', TextType::class, ['attr' => ['class' => 'input']])
+            ->add('dateNaissanceStagiaire', DateTimeType::class, ['attr' => ['class' => 'input']])
+            ->add('sexeStagiaire', ChoiceType::class, [
+                'choices' => [
+                    'Homme' => 'homme',
+                    'Femme' => 'femme',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+            ])
+            ->add('villeStagiaire', TextType::class, ['attr' => ['class' => 'input']])
+            ->add('cpStagiaire', NumberType::class, ['attr' => ['class' => 'input']])
+            ->add('adresseStagiaire', TextType::class, ['attr' => ['class' => 'input']])
+            ->add('mailStagiaire', TextType::class, ['attr' => ['class' => 'input']])
+            ->add('telephoneStagiaire', NumberType::class, ['attr' => ['class' => 'input']])
+            ->add('sessions', EntityType::class, [
+                'class' => Session::class, 'attr' => ['class' => 'form-control']
+            ])
+            ->add('submit', SubmitType::class, ['attr' => ['class' => 'btn btn-success'], 'label' => 'ok']);;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
