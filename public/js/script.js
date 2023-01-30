c69ad0d04b33507434cee35b8f5c1bb09263b251
mohamed67210 <mohamed_bechri@outlook.fr>
2023-01-30 11:47:10 +0100

    debug + dark mode

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,6 @@ var links = document.getElementsByClassName('link')
 var menuBg = document.getElementById("burger_top")
 var menuBgb = document.getElementById("burger_bottom")
 
-console.log(links)
 burgerMenu.addEventListener('click', function () {
     this.classList.toggle("close");
     menu.classList.toggle("open");
@@ -19,5 +18,40 @@ for (let i = 0; i < links.length; i++) {
         console.log('clicker')
         menu.classList.toggle('open');
     })
-
 }
+
+// dark mode
+let sun = document.querySelector('.fa-sun');
+let moon = document.querySelector('.fa-moon')
+let mode = document.getElementById('mode')
+let body = document.querySelector('body')
+
+mode.addEventListener('click', function () {
+
+    if (body.classList.contains('light')) {
+        this.classList.remove('dark')
+        body.classList.remove('light');
+        sun.classList.remove('close')
+        moon.classList.remove('open')
+        localStorage.setItem("theme", "dark");
+
+    } else {
+        this.classList.add('dark')
+        body.classList.add('light');
+        sun.classList.add('close')
+        moon.classList.add('open')
+        localStorage.setItem("theme", "light");
+
+    }
+})
+// si le theme est light
+if (localStorage.getItem("theme") === "light") {
+    body.classList.add('light')
+    mode.classList.add('dark')
+    sun.classList.add('close')
+    moon.classList.add('open')
+    console.log('light')
+}
+
+
+
